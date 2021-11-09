424c323238ede0159bb3d640a119ca2a089ff31a
zack pathounourack <zackpathou@gmail.com>
2021-11-10 00:21:53 +0100

    PROJET APPREACT NATIVE

--- App.js
+++ App.js
@@ -1,21 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+
+import {
+  TeslaModelS,
+  TeslaModel3,
+  TeslaModelX,
+  TeslaModelY,
+} from "./component/tesla_screen";
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Drawer.Navigator
+        drawerPosition="right"
+        initialRouteName="ModelS"
+        screenOptions={{ headerShown: false }}
+      >
+        <Drawer.Screen name="ModelS" component={TeslaModelS} />
+        <Drawer.Screen name="Model3" component={TeslaModel3} />
+        <Drawer.Screen name="ModelX" component={TeslaModelX} />
+        <Drawer.Screen name="ModelY" component={TeslaModelY} />
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
